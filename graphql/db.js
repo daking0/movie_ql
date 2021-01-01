52b40043a3a3db3ec3d17ba29b9793b16c2d6e00
daking0 <angk226@gmail.com>
2021-01-01 20:05:29 +0900

    rest를 graphql로 감싸기

--- graphql/db.js
+++ graphql/db.js
@@ -1,50 +1,7 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Nivolas",
-        score :1
-    },{
-        id: 1,
-        name: "Star Wars - The new one",
-        score : 8
-    },{
-        id: 2,
-        name: "Avengers - the new one",
-        score : 99
-    },{
-        id: 3,
-        name: "The Godfather",
-        score : 83
-    },{
-        id: 4,
-        name: "Logan",
-        score : 2
-    }
-];
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id : movies.length + 1,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
-
-export const deleteMovie = id => {
-    const cleanMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanMovies.length){
-        movies = cleanMovies;
-        return true;
-    }else {
-        return false;
-    }
-}
+export const getMovies = (limit, rating) => 
+   fetch(`${API_URL}`)
+        .then(res => res.json())
+        .then(json => json.data.movies);
