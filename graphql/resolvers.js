c010d642fb72e3960e63eeea2ac2e4667e126e90
daking0 <angk226@gmail.com>
2021-01-01 16:30:13 +0900

    extending Schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,36 @@
 //graphql에서 보낸 쿼리 resolve 필요
+const people = [
+{
+    id: "1",
+    name: "Nivolas",
+    age:18,
+    gender : "female"
+},{
+    id: "2",
+    name: "Deal",
+    age:26,
+    gender : "male"
+},{
+    id: "3",
+    name: "Nicol",
+    age:24,
+    gender : "female"
+},{
+    id: "4",
+    name: "Peter",
+    age:57,
+    gender : "male"
+},{
+    id: "5",
+    name: "Hanna",
+    age:18,
+    gender : "female"
+}
+];
 
 const resolvers={
     Query: {
-        name :() => "nicolas"
+        people :() => people
     }
 };
 
