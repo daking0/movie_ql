4dd4bf9c19c53f923207b5286c1170b7f7984ba5
daking0 <angk226@gmail.com>
2021-01-01 20:27:31 +0900

    finish

--- graphql/db.js
+++ graphql/db.js
@@ -1,7 +1,16 @@
-import fetch from "node-fetch";
-const API_URL = "https://yts.mx/api/v2/list_movies.json";
+import fetch, { Request } from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getMovies = (limit, rating) => 
-   fetch(`${API_URL}`)
-        .then(res => res.json())
-        .then(json => json.data.movies);
+export const getMovies = (limit, rating) => {
+    let REQUESE_URL = API_URL;
+    if(limit > 0){
+        REQUESE_URL += `limit=${limit}`;
+    }
+    if(rating > 0){
+        REQUESE_URL += `&minimum_rating=${rating}`;
+    }
+
+    return fetch(REQUESE_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,7 +3,7 @@ import { getMovies } from "./db";
 
 const resolvers = {
     Query: {
-        movies :() => getMovies()
+        movies :(_,{limit, rating}) => getMovies(limit, rating)
     }
 };
 
